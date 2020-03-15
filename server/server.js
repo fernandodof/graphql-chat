67c3d7cb744fafa2a621186b03b584f5745c4226
Fernando Ferreira <fernandodof@gmail.com>
2020-03-15 09:37:33 +0100

    finishing resolvers for pubSub

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 const { ApolloServer } = require('apollo-server-express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -8,36 +9,38 @@ const jwt = require('jsonwebtoken');
 const db = require('./db');
 
 const port = 9000;
-const jwtSecret = Buffer.from('xkMBdsE+P6242Z2dPV3RD91BPbLIko7t', 'base64');
+const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
 const app = express();
 app.use(cors(), bodyParser.json(), expressJwt({
-  credentialsRequired: false,
-  secret: jwtSecret
+	secret: jwtSecret,
+	credentialsRequired: false
 }));
 
-const typeDefs = fs.readFileSync('./schema.graphql', {encoding: 'utf8'});
+const typeDefs = fs.readFileSync('./schema.graphql', { encoding: 'utf8' });
 const resolvers = require('./resolvers');
 
-function context({req}) {
-  if (req && req.user) {
-    return {userId: req.user.sub};
-  }
-  return {};
+function context({ req }) {
+	if (req && req.user) {
+		return { userId: req.user.sub };
+	}
+	return {};
 }
 
-const apolloServer = new ApolloServer({typeDefs, resolvers, context});
-apolloServer.applyMiddleware({app, path: '/graphql'});
+const apolloServer = new ApolloServer({ typeDefs, resolvers, context });
+apolloServer.applyMiddleware({ app, path: '/graphql' });
 
 app.post('/login', (req, res) => {
-  const {name, password} = req.body;
-  const user = db.users.get(name);
-  if (!(user && user.password === password)) {
-    res.sendStatus(401);
-    return;
-  }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
+	const { name, password } = req.body;
+	const user = db.users.get(name);
+	if (!(user && user.password === password)) {
+		res.sendStatus(401);
+		return;
+	}
+	const token = jwt.sign({ sub: user.id }, jwtSecret);
+	res.send({ token });
 });
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const httpServer = http.createServer(app);
+apolloServer.installSubscriptionHandlers(httpServer);
+httpServer.listen(port, () => console.log(`Server started on port ${port}`));
